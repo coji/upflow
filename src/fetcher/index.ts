5da8944e01c71c59701dc4c5f2865a4955cf4506
coji <coji@techtalk.jp>
2022-06-26 18:04:14 +0900

    refactor: simplify project id specification

--- src/fetcher/index.ts
+++ src/fetcher/index.ts
@@ -1,32 +1,35 @@
 import { Types } from '@gitbeaker/node'
 
-export const fetchFactory = (api: any) => ({
-  /**
-   * MRのコミットリストを取得
-   * @param mergerequestIid
-   * @returns 指定したMRのコミット情報の配列
-   */
-  commits: async (mergerequestIid: number) =>
-    (await api.MergeRequests.commits(
-      process.env.PROJECT_ID!,
-      mergerequestIid
-    )) as Types.CommitSchema[],
-  /**
-   * MRのディスカッションリストを取得
-   * @param mergerequestIid
-   * @returns 指定したMRのディスカッション情報の配列
-   */
-  discussions: async (mergerequestIid: number) =>
-    (await api.MergeRequestDiscussions.all(
-      process.env.PROJECT_ID!,
-      mergerequestIid
-    )) as Types.DiscussionSchema[],
-  /**
-   * プロジェクトのすべてのMR情報を取得
-   * @returns プロジェクトのすべてのMR情報の配列
-   */
-  mergerequests: async () =>
-    (await api.MergeRequests.all({
-      projectId: process.env.PROJECT_ID!,
-    })) as Types.MergeRequestSchema[],
-})
+export const fetchFactory = (api: any) => {
+  const projectId = process.env.PROJECT_ID
+  return {
+    /**
+     * MRのコミットリストを取得
+     * @param mergerequestIid
+     * @returns 指定したMRのコミット情報の配列
+     */
+    commits: async (mergerequestIid: number) =>
+      (await api.MergeRequests.commits(
+        projectId,
+        mergerequestIid
+      )) as Types.CommitSchema[],
+    /**
+     * MRのディスカッションリストを取得
+     * @param mergerequestIid
+     * @returns 指定したMRのディスカッション情報の配列
+     */
+    discussions: async (mergerequestIid: number) =>
+      (await api.MergeRequestDiscussions.all(
+        projectId,
+        mergerequestIid
+      )) as Types.DiscussionSchema[],
+    /**
+     * プロジェクトのすべてのMR情報を取得
+     * @returns プロジェクトのすべてのMR情報の配列
+     */
+    mergerequests: async () =>
+      (await api.MergeRequests.all({
+        projectId,
+      })) as Types.MergeRequestSchema[],
+  }
+}
