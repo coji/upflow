2f98b89ce31c9658b6016351071ad145d67d0e69
coji <coji@techtalk.jp>
2022-07-19 13:04:21 +0900

    fix remix

--- front-nextjs/src/features/mergerequests/hooks/useMergeRequests.ts
+++
@@ -1,5 +0,0 @@
-import { useQuery } from 'react-query'
-import ky from 'ky'
-import { MergeRequest } from '@prisma/client'
-
-export const useMergeRequests = () => useQuery(['mergerequests'], async () => ky.get('/api/mergerequests/list').json<{ items: MergeRequest[] }>())
