49039cd17b67f9320a7cc4e9e153970b50c4638e
coji <coji@techtalk.jp>
2022-10-19 18:40:48 +0900

    default button text

--- app/components/form/AppSubmitButton.tsx
+++ app/components/form/AppSubmitButton.tsx
@@ -3,13 +3,13 @@ import type { ButtonProps } from '@chakra-ui/react'
 import { Button } from '@chakra-ui/react'
 
 interface AppSubmitButtonProps extends ButtonProps {
-  children: React.ReactNode
+  children?: React.ReactNode
 }
 export const AppSubmitButton = ({ form, children, ...rest }: AppSubmitButtonProps) => {
   const isSubmitting = useIsSubmitting(form)
   return (
     <Button type="submit" disabled={isSubmitting} form={form} {...rest}>
-      {isSubmitting ? 'Submitting...' : children}
+      {isSubmitting ? 'Submitting...' : children ?? 'Submit'}
     </Button>
   )
 }
