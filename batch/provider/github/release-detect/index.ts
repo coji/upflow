18655ec26ee502ef41885f83cf86c31f1e4dde27
coji <coji@techtalk.jp>
2023-07-07 16:49:41 +0900

    feat: deploy detect by tag

--- batch/provider/github/release-detect/index.ts
+++ batch/provider/github/release-detect/index.ts
@@ -1,26 +1,82 @@
 import type { ShapedGitHubPullRequest } from '../model'
 import type { createStore } from '../store'
 import * as R from 'remeda'
+import dayjs from '~/app/libs/dayjs'
 
-const releasedPullRequests = (allPullRequests: ShapedGitHubPullRequest[], key: string) => {
+const mergedPullRequests = (allPullRequests: ShapedGitHubPullRequest[], targetBranch: string) => {
   return R.pipe(
     allPullRequests,
-    R.filter((pr) => pr.targetBranch === key && pr.state === 'closed' && pr.mergedAt !== null),
+    R.filter((pr) => pr.targetBranch === targetBranch && pr.state === 'closed' && pr.mergedAt !== null), // 一旦mainブランチ固定
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     R.sortBy((pr) => pr.mergedAt!),
   )
+}
+
+/**
+ * ブランチ基準でリリース日を探す
+ * @param allPullRequests
+ * @param store
+ * @param pr
+ * @param branch
+ * @returns
+ */
+const findReleaseDateByBranch = async (
+  allPullRequests: ShapedGitHubPullRequest[],
+  store: ReturnType<typeof createStore>,
+  pr: ShapedGitHubPullRequest,
+  branch: string,
+) => {
+  for (const m of mergedPullRequests(allPullRequests, branch)) {
+    if ((await store.loader.commits(m.number)).some((c) => c.sha === pr.mergeCommitSha)) {
+      return m.mergedAt
+    }
+  }
+  return null
+}
+
+/**
+ * ブランチ基準でリリース日を探す
+ * @param allPullRequests
+ * @param store
+ * @param pr
+ * @param key
+ * @returns
+ */
+const findReleaseDateByTag = async (
+  allPullRequests: ShapedGitHubPullRequest[],
+  store: ReturnType<typeof createStore>,
+  pr: ShapedGitHubPullRequest,
+  tagCondition: string,
+) => {
+  if (pr.mergedAt === null) return null
+
+  const tagRegexp = new RegExp(tagCondition)
+  const allReleaseTags = (await store.loader.tags())
+    .filter((t) => tagRegexp.test(t.name)) // リリース用のタグを抽出
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    .sort((a, b) => dayjs(a.committedAt!).unix() - dayjs(b.committedAt!).unix()) // 古い順に並べる
+
+  for (const releaseTag of allReleaseTags) {
+    if (dayjs(pr.mergedAt).unix() <= dayjs(releaseTag.committedAt).unix()) {
+      return releaseTag.committedAt
+    }
+  }
+  return null
 }
 
 export const findReleaseDate = async (
   allPullRequests: ShapedGitHubPullRequest[],
   store: ReturnType<typeof createStore>,
   pr: ShapedGitHubPullRequest,
-  key: string,
+  releaseDetectionMethod: string,
+  releaseDetectionKey: string,
 ) => {
-  for (const m of releasedPullRequests(allPullRequests, key)) {
-    if ((await store.loader.commits(m.number)).some((c) => c.sha === pr.mergeCommitSha)) {
-      return m.mergedAt
-    }
+  console.log(pr.number, { releaseDetectionMethod, releaseDetectionKey })
+  if (releaseDetectionMethod === 'branch') {
+    return findReleaseDateByBranch(allPullRequests, store, pr, releaseDetectionKey)
+  }
+  if (releaseDetectionMethod === 'tags') {
+    return findReleaseDateByTag(allPullRequests, store, pr, releaseDetectionKey)
   }
   return null
 }
