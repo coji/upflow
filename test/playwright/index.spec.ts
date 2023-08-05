cdc99cef23cbb4e88fade12c3a02249de46bf252
coji <coji@techtalk.jp>
2023-08-05 20:05:21 +0900

    fix: playwright test

--- test/playwright/index.spec.ts
+++ test/playwright/index.spec.ts
@@ -20,14 +20,3 @@ test('"UpFlow"タイトル表示', async ({ page }) => {
   await page.goto('/')
   await expect(page.locator('text=UpFlow')).toBeVisible()
 })
-
-test('ログインして admin 画面に遷移', async ({ page }) => {
-  await page.goto('/login')
-
-  await page.getByLabel('Eメール').fill(testUser.email)
-  await page.getByRole('button', { name: 'ログイン' }).click()
-  await page.waitForURL('/')
-
-  await expect(page.getByRole('link', { name: 'UpFlow' })).toHaveAttribute('href', '/admin')
-  await expect(page.getByText('admin')).toBeVisible()
-})
