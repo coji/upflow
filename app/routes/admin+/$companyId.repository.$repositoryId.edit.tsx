38b99bb403a32c92ffadf91af8415b7505c10872
coji <coji@techtalk.jp>
2023-08-07 21:51:17 +0900

    refactor: shadcn/ui

--- app/routes/admin+/$companyId.repository.$repositoryId.edit.tsx
+++ app/routes/admin+/$companyId.repository.$repositoryId.edit.tsx
@@ -1,90 +1,157 @@
-import { redirect, type ActionArgs, type LoaderArgs } from '@remix-run/node'
+import { conform, useForm } from '@conform-to/react'
+import { parse } from '@conform-to/zod'
+import { json, redirect, type ActionArgs, type LoaderArgs } from '@remix-run/node'
 import { Form, Link, useLoaderData } from '@remix-run/react'
-import invariant from 'tiny-invariant'
 import { match } from 'ts-pattern'
+import { z } from 'zod'
+import { zx } from 'zodix'
 import { AppProviderBadge } from '~/app/components'
 import { Button, Card, CardContent, CardFooter, CardHeader, CardTitle, Input, Label, Stack } from '~/app/components/ui'
 import { getRepository, updateRepository } from '~/app/models/admin/repository.server'
 
+const githubSchema = z.object({
+  provider: z.literal('github'),
+  owner: z.string().nonempty(),
+  repo: z.string().nonempty(),
+  releaseDetectionMethod: z.enum(['branch', 'tags']),
+  releaseDetectionKey: z.string().nonempty(),
+})
+
+const gitlabSchema = z.object({
+  provider: z.literal('gitlab'),
+  projectId: z.string().nonempty(),
+  releaseDetectionMethod: z.enum(['branch', 'tags']),
+  releaseDetectionKey: z.string().nonempty(),
+})
+
 export const loader = async ({ request, params }: LoaderArgs) => {
-  invariant(params.repositoryId, 'company id should specified')
-  const repository = getRepository(params.repositoryId)
+  const { repositoryId } = zx.parseParams(params, { companyId: z.string(), repositoryId: z.string() })
+  const repository = await getRepository(repositoryId)
   if (!repository) {
     throw new Error('repository not found')
   }
-  return repository
+  if (!repository.integration) {
+    throw new Error('repository.integration not found')
+  }
+  return json({ repository, provider: repository.integration.provider })
 }
 
 export const action = async ({ request, params }: ActionArgs) => {
+  const { companyId, repositoryId } = zx.parseParams(params, { companyId: z.string(), repositoryId: z.string() })
   const formData = await request.formData()
-  invariant(params.repositoryId, 'repository id should specified')
   const entries = Object.fromEntries(formData.entries())
-  const repository = await updateRepository(params.repositoryId, entries)
-  if (repository) {
-    return redirect(`/admin/${params.companyId}`)
-  }
-  return null
+  await updateRepository(repositoryId, entries)
+  return redirect(`/admin/${companyId}`)
+}
+
+const GithubRepositoryForm = ({
+  repository,
+}: {
+  repository: NonNullable<Awaited<ReturnType<typeof getRepository>>>
+}) => {
+  const [form, { owner, repo, releaseDetectionKey, releaseDetectionMethod }] = useForm({
+    id: 'repository-edit-form',
+    onValidate: ({ formData }) => parse(formData, { schema: githubSchema }),
+    defaultValue: repository,
+  })
+
+  return (
+    <Form method="POST" {...form.props}>
+      <Stack>
+        <fieldset>
+          <AppProviderBadge provider="github" />
+          <input type="hidden" name="provider" value="github" />
+        </fieldset>
+
+        <fieldset>
+          <Label htmlFor={owner.id}>Owner</Label>
+          <Input {...conform.input(owner)} />
+          <div className="text-destructive">{owner.error}</div>
+        </fieldset>
+
+        <fieldset>
+          <Label htmlFor={repo.id}>Repo</Label>
+          <Input {...conform.input(repo)} />
+          <div className="text-destructive">{repo.error}</div>
+        </fieldset>
+
+        <fieldset>
+          <Label htmlFor={releaseDetectionMethod.id}>Release Detection Method</Label>
+          <Input {...conform.input(releaseDetectionMethod)} />
+          <div className="text-destructive">{releaseDetectionMethod.error}</div>
+        </fieldset>
+
+        <fieldset>
+          <Label htmlFor={releaseDetectionKey.id}>Release Detection Key</Label>
+          <Input {...conform.input(releaseDetectionKey)} />
+          <div className="text-destructive">{releaseDetectionKey.error}</div>
+        </fieldset>
+      </Stack>
+    </Form>
+  )
+}
+
+const GitLabRepositoryForm = ({
+  repository,
+}: {
+  repository: NonNullable<Awaited<ReturnType<typeof getRepository>>>
+}) => {
+  const [form, { projectId, releaseDetectionKey, releaseDetectionMethod }] = useForm({
+    id: 'repository-edit-form',
+    onValidate: ({ formData }) => parse(formData, { schema: gitlabSchema }),
+    defaultValue: repository,
+  })
+
+  return (
+    <Form method="POST" {...form.props}>
+      <Stack>
+        <fieldset>
+          <AppProviderBadge provider="gitlab" />
+          <input type="hidden" name="provider" value="gitlab" />
+        </fieldset>
+
+        <fieldset>
+          <Label htmlFor={projectId.id}>ProjectID</Label>
+          <Input {...conform.input(projectId)} />
+          <div className="text-destructive">{projectId.error}</div>
+        </fieldset>
+
+        <fieldset>
+          <Label htmlFor="releaseDetectionMethod">Release Detection Method</Label>
+          <Input {...conform.input(releaseDetectionMethod)} />
+          <div className="text-destructive">{releaseDetectionMethod.error}</div>
+        </fieldset>
+
+        <fieldset>
+          <Label htmlFor="releaseDetectionKey">Release Detection Key</Label>
+          <Input {...conform.input(releaseDetectionKey)} />
+          <div className="text-destructive">{releaseDetectionKey.error}</div>
+        </fieldset>
+      </Stack>
+    </Form>
+  )
 }
 
 const EditRepositoryModal = () => {
-  const repository = useLoaderData<typeof loader>()
-  if (!repository) {
-    return <div>repository not found</div>
-  }
+  const { repository, provider } = useLoaderData<typeof loader>()
+  const form = match(provider)
+    .with('github', () => <GithubRepositoryForm repository={repository} />)
+    .with('gitlab', () => <GitLabRepositoryForm repository={repository} />)
+    .otherwise(() => <></>)
 
   return (
     <Card>
       <CardHeader>
         <CardTitle>Edit Repository</CardTitle>
       </CardHeader>
-      <CardContent>
-        <Form method="POST" id="repository-edit-form">
-          <div className="grid grid-cols-[auto_1fr] items-center gap-4">
-            <div className="col-span-2">
-              <AppProviderBadge provider={repository.integration.provider} />
-            </div>
-
-            {match(repository.integration.provider)
-              .with('github', () => (
-                <>
-                  <Label htmlFor="owner">Owner</Label>
-                  <Input name="owner" id="owner" defaultValue={repository.owner ?? ''} />
-
-                  <Label htmlFor="repo">Repo</Label>
-                  <Input name="repo" id="repo" defaultValue={repository.repo ?? ''} />
-                </>
-              ))
-              .with('gitlab', () => (
-                <>
-                  <Label htmlFor="projectId">ProjectID</Label>
-                  <Input name="projectId" id="projectId" autoFocus defaultValue={repository.projectId ?? ''} />
-                </>
-              ))
-              .otherwise(() => (
-                <></>
-              ))}
-
-            <Label htmlFor="releaseDetectionMethod">Release Detection Method</Label>
-            <Input
-              name="releaseDetectionMethod"
-              id="releaseDetectionMethod"
-              defaultValue={repository.releaseDetectionMethod}
-            />
-
-            <Label htmlFor="releaseDetectionKey">Release Detection Key</Label>
-            <Input name="releaseDetectionKey" id="releaseDetectionKey" defaultValue={repository.releaseDetectionKey} />
-          </div>
-        </Form>
-      </CardContent>
+      <CardContent>{form}</CardContent>
       <CardFooter>
         <Stack direction="row">
           <Button type="submit" form="repository-edit-form">
             Update
           </Button>
           <Button asChild variant="ghost">
-            <Link to=".." preventScrollReset>
-              Cancel
-            </Link>
+            <Link to="..">Cancel</Link>
           </Button>
         </Stack>
       </CardFooter>
