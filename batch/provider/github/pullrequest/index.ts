d22d8b913ddfa47a77b50d16862fb9fc7e31d3dc
coji <coji@techtalk.jp>
2023-08-10 15:52:10 +0900

    fix: コメント履歴からbotとPR作成者自身を除外して計算する

--- batch/provider/github/pullrequest/index.ts
+++ batch/provider/github/pullrequest/index.ts
@@ -1,12 +1,12 @@
-import type { ShapedGitHubPullRequest } from '../model'
 import type { PullRequest } from '@prisma/client'
-import dayjs from '~/app/libs/dayjs'
-import { createStore } from '../store'
-import { codingTime, pickupTime, reviewTime, deployTime, totalTime } from '~/batch/bizlogic/cycletime'
-import { findReleaseDate } from '../release-detect'
 import { first } from 'remeda'
+import dayjs from '~/app/libs/dayjs'
+import { codingTime, deployTime, pickupTime, reviewTime, totalTime } from '~/batch/bizlogic/cycletime'
 import { logger } from '~/batch/helper/logger'
+import type { ShapedGitHubPullRequest } from '../model'
+import { findReleaseDate } from '../release-detect'
 import { analyzeReviewResponse } from '../review-response'
+import { createStore } from '../store'
 
 const nullOrDate = (dateStr?: Date | string | null) => {
   return dateStr ? dayjs(dateStr).utc().toISOString() : null
@@ -23,9 +23,17 @@ export const buildPullRequests = async (
     []
   for (const pr of pullrequests) {
     try {
+      // コミット履歴
       const commits = await store.loader.commits(pr.number)
-      const reviews = await store.loader.reviews(pr.number)
-      const discussions = await store.loader.discussions(pr.number)
+
+      // レビュー履歴 (bot と PR 作成者は除外)
+      const reviews = (await store.loader.reviews(pr.number)).filter(
+        (r) => !r.user?.endsWith('[bot]') && r.user !== pr.author,
+      )
+      // コメント履歴 (bot と PR 作成者は除外)
+      const discussions = (await store.loader.discussions(pr.number)).filter(
+        (d) => !d.user.endsWith('[bot]') && d.user !== pr.author,
+      )
 
       reviewResponses.push(
         ...analyzeReviewResponse(discussions).map((res) => ({
@@ -37,10 +45,19 @@ export const buildPullRequests = async (
         })),
       )
 
+      // 初期コミット日時
       const firstCommittedAt = nullOrDate(commits.length > 0 ? commits[0].date : null)
+
+      // プルリク作成日時
       const pullRequestCreatedAt = nullOrDate(pr.createdAt) ?? ''
+
+      // レビュー開始日時
       const firstReviewedAt = nullOrDate(first(discussions)?.createdAt ?? first(reviews)?.submittedAt) // レビュー開始 = コメント最新 or レビュー submit 最新
+
+      // マージ日時
       const mergedAt = nullOrDate(pr.mergedAt)
+
+      // リリース日時
       const releasedAt =
         pr.mergedAt && pr.mergeCommitSha
           ? await findReleaseDate(pullrequests, store, pr, config.releaseDetectionMethod, config.releaseDetectionKey)
