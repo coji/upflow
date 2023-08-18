e0ad401877a9ceba89fb1b6f148b14f8062bec78
coji <coji@techtalk.jp>
2023-08-18 12:20:57 +0900

    feat: show live comments/reviews/commits

--- app/routes/admin+/$companyId.repository.$repositoryId.$pullId.tsx
+++ app/routes/admin+/$companyId.repository.$repositoryId.$pullId.tsx
@@ -1,8 +1,12 @@
 import { json, type LoaderArgs } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
+import invariant from 'tiny-invariant'
 import { z } from 'zod'
 import { zx } from 'zodix'
+import { Heading, Stack } from '~/app/components/ui'
 import { getPullRequest } from '~/app/models/admin/pull-requests.server'
+import { getRepository } from '~/app/models/admin/repository.server'
+import { createFetcher } from '~/batch/provider/github/fetcher'
 
 export const handle = {
   breadcrumb: ({
@@ -25,17 +29,54 @@ export const loader = async ({ request, params }: LoaderArgs) => {
     repositoryId: z.string(),
     pullId: zx.NumAsString,
   })
+  const repository = await getRepository(repositoryId)
+  if (!repository) {
+    throw new Error('Repository not found')
+  }
+  invariant(repository.owner, 'Repository is not integrated')
+  invariant(repository.repo, 'Repository is not integrated')
+  invariant(repository.integration, 'Repository is not integrated')
+  invariant(repository.integration.privateToken, 'Repository is not integrated')
+
   const pull = await getPullRequest(pullId)
-  return json({ companyId, repositoryId, pull })
+
+  const fetcher = createFetcher({
+    owner: repository.owner,
+    repo: repository.repo,
+    token: repository.integration.privateToken,
+  })
+
+  const commits = await fetcher.commits(pullId)
+  const comments = await fetcher.reviewComments(pullId)
+  const reviews = await fetcher.reviews(pullId)
+  return json({ companyId, repositoryId, pull, commits, comments, reviews })
 }
 
 const RepositoryPullsIndexPage = () => {
-  const { pull } = useLoaderData<typeof loader>()
+  const { pull, comments, reviews, commits } = useLoaderData<typeof loader>()
 
   return (
-    <div>
-      <pre>{JSON.stringify(pull, null, 2)}</pre>
-    </div>
+    <Stack>
+      <div>
+        <Heading>Pull Request Details</Heading>
+        <pre>{JSON.stringify(pull, null, 2)}</pre>
+      </div>
+
+      <div>
+        <Heading>Comments (live)</Heading>
+        <pre>{JSON.stringify(comments, null, 2)}</pre>
+      </div>
+
+      <div>
+        <Heading>Reviews (live)</Heading>
+        <pre>{JSON.stringify(reviews, null, 2)}</pre>
+      </div>
+
+      <div>
+        <Heading>Commits (live)</Heading>
+        <pre>{JSON.stringify(commits, null, 2)}</pre>
+      </div>
+    </Stack>
   )
 }
 export default RepositoryPullsIndexPage
