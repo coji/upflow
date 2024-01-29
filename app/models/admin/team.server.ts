ceff9dbfa2be63b063785850706e09f7a4feec0a
coji <coji@techtalk.jp>
2024-01-29 18:12:18 +0900

    Add team-related models and routes

--- app/models/admin/team.server.ts
+++ app/models/admin/team.server.ts
@@ -4,5 +4,19 @@ import { prisma } from '~/app/services/db.server'
 export const listTeams = async (companyId: Company['id']) => {
   return await prisma.team.findMany({
     where: { companyId },
+    include: {
+      _count: {
+        select: {
+          TeamRepository: true,
+          teamUser: true,
+        },
+      },
+    },
+  })
+}
+
+export const getTeam = async (id: string) => {
+  return await prisma.team.findUnique({
+    where: { id },
   })
 }
