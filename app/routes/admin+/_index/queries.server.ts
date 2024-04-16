195cc0759769986c64ec9200cca6ca7e6d328611
coji <coji@techtalk.jp>
2024-04-16 20:33:28 +0900

    Refactor listCompanies function and update file paths

--- app/routes/admin+/_index/queries.server.ts
+++ app/routes/admin+/_index/queries.server.ts
@@ -1,9 +1,5 @@
 import { db } from '~/app/services/db.server'
 
 export const listCompanies = async () => {
-  return await db
-    .selectFrom('companies')
-    .innerJoin('teams', 'companies.id', 'teams.company_id')
-    .select(['companies.id', 'companies.name', 'teams.id', 'teams.name'])
-    .execute()
+  return await db.selectFrom('companies').select(['id', 'name']).execute()
 }
--- app/routes/admin+/_layout/queries.server.ts
+++ app/routes/admin+/_layout/queries.server.ts
@@ -3,7 +3,6 @@ import { db } from '~/app/services/db.server'
 export const listCompanies = async () => {
   return await db
     .selectFrom('companies')
-    .innerJoin('teams', 'companies.id', 'teams.company_id')
-    .select(['companies.id', 'companies.name', 'teams.id', 'teams.name'])
+    .select(['companies.id', 'companies.name'])
     .execute()
 }
