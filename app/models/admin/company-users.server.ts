27248b66416d09caea6ef0afba846043d6895f9a
coji <coji@techtalk.jp>
2024-04-16 19:13:11 +0900

    Refactor listCompanyUsers function to use db server instead of prisma

--- app/models/admin/company-users.server.ts
+++ app/models/admin/company-users.server.ts
@@ -1,9 +1,11 @@
-import { prisma } from '~/app/services/db.server'
+import { db } from '~/app/services/db.server'
 
 export const listCompanyUsers = async (companyId: string) => {
-  return await prisma.companyUser.findMany({
-    where: { companyId },
-    include: { user: true },
-    orderBy: { createdAt: 'asc' },
-  })
+  return await db
+    .selectFrom('company_users')
+    .innerJoin('users', 'company_users.user_id', 'users.id')
+    .selectAll()
+    .where('company_id', '==', companyId)
+    .orderBy('created_at', 'asc')
+    .execute()
 }
