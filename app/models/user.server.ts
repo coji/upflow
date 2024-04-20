7eddfe7d6f24259ecc2a498a6ebae1971cd91152
coji <coji@techtalk.jp>
2024-04-20 21:20:09 +0900

    Refactor and remove unused code in user.server.ts and company.server.ts

--- app/models/user.server.ts
+++ app/models/user.server.ts
@@ -2,49 +2,31 @@ import type { User } from '@prisma/client'
 import { prisma } from '~/app/services/db.server'
 export type { User } from '@prisma/client'
 
-export async function getUserById(id: User['id']) {
-  return await prisma.user.findUnique({
-    where: { id },
-    select: {
-      id: true,
-      email: true,
-      displayName: true,
-      pictureUrl: true,
-      locale: true,
-      role: true,
-    },
-  })
-}
-
-export async function getUserByEmail({ email }: { email: User['email'] }) {
-  return await prisma.user.findUniqueOrThrow({ where: { email } })
-}
-
-export async function upsertUserByEmail({
-  email,
-  displayName,
-  pictureUrl,
-  locale,
-}: {
-  email: User['email']
-  displayName: User['displayName']
-  pictureUrl: User['pictureUrl']
-  locale: User['locale']
-}) {
-  return await prisma.user.upsert({
-    where: { email },
-    select: {
-      id: true,
-      email: true,
-      displayName: true,
-      pictureUrl: true,
-      locale: true,
-      role: true,
-    },
-    create: { email, displayName, pictureUrl, locale },
-    update: { displayName, pictureUrl, locale },
-  })
-}
+// export async function upsertUserByEmail({
+//   email,
+//   displayName,
+//   pictureUrl,
+//   locale,
+// }: {
+//   email: User['email']
+//   displayName: User['displayName']
+//   pictureUrl: User['pictureUrl']
+//   locale: User['locale']
+// }) {
+//   return await prisma.user.upsert({
+//     where: { email },
+//     select: {
+//       id: true,
+//       email: true,
+//       displayName: true,
+//       pictureUrl: true,
+//       locale: true,
+//       role: true,
+//     },
+//     create: { email, displayName, pictureUrl, locale },
+//     update: { displayName, pictureUrl, locale },
+//   })
+// }
 
 export async function deleteUserByEmail(email: User['email']) {
   return await prisma.user.delete({ where: { email } })
--- test/playwright/support/create-test-user.ts
+++ test/playwright/support/create-test-user.ts
@@ -1,14 +1,27 @@
-import { upsertUserByEmail } from '~/app/models/user.server'
+import { nanoid } from 'nanoid'
+import { db, sql, type DB, type Insertable } from '~/app/services/db.server'
 
 export const createTestUser = async (
-  props: Parameters<typeof upsertUserByEmail>[0],
+  data: Omit<Insertable<DB.User>, 'id' | 'updatedAt'>,
 ) => {
-  if (!props.email) {
-    throw new Error('email required for login')
-  }
-  if (!props.email.endsWith('@example.com')) {
-    throw new Error('All test emails must end in @example.com')
+  if (!data.email.endsWith('@example.com')) {
+    throw new Error('All test emails should be end in @example.com')
   }
 
-  await upsertUserByEmail(props)
+  await db
+    .insertInto('users')
+    .values({
+      ...data,
+      id: nanoid(),
+      updatedAt: sql`CURRENT_TIMESTAMP`,
+    })
+    .onConflict((cb) =>
+      cb.column('email').doUpdateSet((eb) => ({
+        role: eb.ref('excluded.role'),
+        locale: eb.ref('excluded.locale'),
+        pictureUrl: eb.ref('excluded.pictureUrl'),
+        displayName: eb.ref('excluded.displayName'),
+      })),
+    )
+    .executeTakeFirst()
 }
