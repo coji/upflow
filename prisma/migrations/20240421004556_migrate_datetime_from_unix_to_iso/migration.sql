1d4a29404e3486788a9c6c4261dcb58c55fbaae1
coji <coji@techtalk.jp>
2024-04-21 17:40:38 +0900

    Migrate datetime values from Unix to ISO format

--- prisma/migrations/20240421004556_migrate_datetime_from_unix_to_iso/migration.sql
+++ prisma/migrations/20240421004556_migrate_datetime_from_unix_to_iso/migration.sql
@@ -32,9 +32,9 @@ UPDATE users SET
 	created_at = iif(like('%-%', created_at), created_at, datetime (created_at / 1000, 'unixepoch'));
 
 UPDATE pull_requests SET
-	first_committed_at = strftime('%Y-%M-%d %H:%M:%S', first_committed_at),
-	pull_request_created_at = strftime('%Y-%M-%d %H:%M:%S', pull_request_created_at),
-	first_reviewed_at = strftime('%Y-%M-%d %H:%M:%S', first_reviewed_at),
-	merged_at = strftime('%Y-%M-%d %H:%M:%S', merged_at),
-	released_at = strftime('%Y-%M-%d %H:%M:%S', released_at),
-	updated_at = strftime('%Y-%M-%d %H:%M:%S', updated_at);
+	first_committed_at = strftime('%Y-%m-%d %H:%M:%S', first_committed_at),
+	pull_request_created_at = strftime('%Y-%m-%d %H:%M:%S', pull_request_created_at),
+	first_reviewed_at = strftime('%Y-%m-%d %H:%M:%S', first_reviewed_at),
+	merged_at = strftime('%Y-%m-%d %H:%M:%S', merged_at),
+	released_at = strftime('%Y-%m-%d %H:%M:%S', released_at),
+	updated_at = strftime('%Y-%m-%d %H:%M:%S', updated_at);
