63ff444314b0bf9fa5e7ed3c491b4c362a96770c
coji <coji@techtalk.jp>
2024-05-03 10:31:19 +0900

    Add swap file configuration to fly.toml and remove swap setup from start.sh

--- start.sh
+++ start.sh
@@ -7,14 +7,6 @@
 
 set -ex
 
-# swap on
-fallocate -l 512M /swapfile
-chmod 0600 /swapfile
-mkswap /swapfile
-echo 10 > /proc/sys/vm/swappiness
-swapon /swapfile
-echo 1 > /proc/sys/vm/overcommit_memory
-
 echo "DATABASE_URL is $DATABASE_URL"
 npx prisma migrate deploy
 node server.mjs
