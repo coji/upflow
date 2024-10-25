71c6076d59c81bad94f126e65535d9b1c9eb3ff8
coji <coji@techtalk.jp>
2024-10-25 16:49:34 +0900

    Refactor Google authentication strategy to update redirectURI

--- app/features/auth/services/google-auth.server.ts
+++ app/features/auth/services/google-auth.server.ts
@@ -9,7 +9,7 @@ export const strategy = new GoogleStrategy(
   {
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    redirectURI: `${process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : ''}/auth/google/callback`,
+    redirectURI: `${process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://upflow.team'}/auth/google/callback`,
   },
   verifyUser,
 )
