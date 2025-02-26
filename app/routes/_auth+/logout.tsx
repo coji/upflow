66a240ace6d9ac4cb4bf7bfde6c7357a4debc1a3
coji <coji@techtalk.jp>
2025-02-26 22:23:16 +0900

    feat: update logout route to use href for improved consistency

--- app/routes/_auth+/logout.tsx
+++ app/routes/_auth+/logout.tsx
@@ -1,6 +1,5 @@
 import type React from 'react'
-import { redirect, useFetcher } from 'react-router'
-import { $path } from 'safe-routes'
+import { href, redirect, useFetcher } from 'react-router'
 import { Button, type ButtonProps } from '~/app/components/ui'
 import { getSession, sessionStorage } from '~/app/features/auth/services/auth'
 import type { Route } from './+types/logout'
@@ -9,7 +8,7 @@ export const loader = async ({ request }: Route.LoaderArgs) => {
   const session = await getSession(request)
   const headers = new Headers()
   headers.append('Set-Cookie', await sessionStorage.destroySession(session))
-  throw redirect('/login', { headers })
+  throw redirect(href('/login'), { headers })
 }
 
 interface LogoutButtonProps extends ButtonProps {
@@ -19,7 +18,7 @@ export const LogoutButton = ({ children, ...rest }: LogoutButtonProps) => {
   const fetcher = useFetcher<typeof loader>()
 
   return (
-    <fetcher.Form method="GET" action={$path('/logout')}>
+    <fetcher.Form method="GET" action={href('/logout')}>
       <Button type="submit" variant="outline" {...rest}>
         {children ? children : 'ログアウト'}
       </Button>
