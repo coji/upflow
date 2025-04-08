5527c680732fcf0b25bf0f035b6655a814a63952
coji <coji@techtalk.jp>
2025-04-08 23:10:19 +0900

    refactor: rename deleteCompanyAction to deleteOrganizationAction for consistency

--- app/routes/admin+/$organization.settings/forms/actions.server.ts
+++ app/routes/admin+/$organization.settings/forms/actions.server.ts
@@ -1,4 +1,4 @@
-export { action as deleteCompanyAction } from './delete-organization.action.server'
+export { action as deleteOrganizationAction } from './delete-organization.action.server'
 export { action as exportSettingsAction } from './export-settings.action.server'
 export { action as integrationSettingsAction } from './integration-settings.action.server'
 export { action as organizationSettingsAction } from './organization-settings.action.server'
